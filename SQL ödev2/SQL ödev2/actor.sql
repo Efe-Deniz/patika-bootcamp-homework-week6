fe8f0756947c70be58cc6e9b77f7a03b431c3d1b
Efe-Deniz <dnz.ali.efe@gmail.com>
2025-08-19 02:08:40 +0300

    sql ödev2

---
+++ SQL ödev2/SQL ödev2/actor.sql
@@ -0,0 +1,6 @@
+SELECT first_name,last_name FROM actor
+WHERE first_name IN ('Penelope','Nick','Ed');
+
+
+
+
---
+++ SQL ödev2/SQL ödev2/replacement_cost.sql
@@ -0,0 +1,6 @@
+SELECT * FROM film
+WHERE replacement_cost BETWEEN 12.99 AND 16.99;
+
+
+
+
